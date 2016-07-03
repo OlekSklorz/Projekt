2ad6c68c363955274d49e9d17e81b172b8b0999d
KonradStachon <konradstachon95@gmail.com>
2016-07-03 14:20:55 +0200

    Update MainMenuFrame.java

--- MainMenuFrame.java
+++ MainMenuFrame.java
@@ -8,7 +8,6 @@ import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.GridBagConstraints;
 import javax.swing.JLabel;
-import game.GameFont;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
