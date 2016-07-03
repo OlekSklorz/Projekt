a887adcb014e4b63a93304e5e939e0649e95094a
OlekSklorz <olek1995@poczta.onet.pl>
2016-07-03 15:11:29 +0200

    Update User.java

--- User.java
+++ User.java
@@ -2,12 +2,12 @@ package game;
 // DOKOŃCZĘ POTEM 
 public class User { // tą klasę zastosuję w MainMenuFrame w przyciskach logowania i rejestracji 
     private String nick = "Anonim";
-    public void signIn(){
+    public String signIn(){
         // Tu wyświetla ramkę z pytaniem o użytkownika i hasło
         // sprawdza czy taki użytkownik istnieje w pliku i czy hasło poprawne
         // jeśli tak - loguje i zmienia nick, jeśli nie - bład i pozostaje anonimowy 
     }
-    public String signUp(){
+    public void signUp(){
         // Tu wyświetla ramkę z pytaniem o użytkownika, hasło i potwierdzenie hasła 
         // sprawdza czy takiego użytkownika jeszcze nie ma. W między czasie działa też szyfrowanie
         // jeśli nikogo takiego nie ma - zapisuje do pliku, inaczej bład 
