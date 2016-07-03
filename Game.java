225206a3774c62bb4ccb10e066177283f9817967
KonradStachon <konradstachon95@gmail.com>
2016-07-03 13:30:20 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -5,7 +5,7 @@ import javax.swing.WindowConstants;
  * Klasa <code>Game</code> reprezentuje główną klasę gry.
  */
 public class Game {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //Konrad
         EventQueue.invokeLater(() -> new MainMenuFrame() {{
             setTitle("Zestaw gier");
             setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
