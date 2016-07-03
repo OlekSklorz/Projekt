8391855dbc3340bd42c8366362fa5d7ed5d7d747
OlekSklorz <olek1995@poczta.onet.pl>
2016-07-03 12:57:55 +0200

    Update GameFont.java
    dodanie komentarzy

--- GameFont.java
+++ GameFont.java
@@ -1,8 +1,16 @@
 package game;
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
+/**
+ * Klasa <code>GameFont</code> reprezentuje czcionkę używaną w grze. 
+ * Zawiera dwa warianty czcionki - artystyczną oraz jeśli system jej nie posiada - zwykłą.
+ */
 public class GameFont {
     private static final Font font = new Font("Serif", Font.BOLD, 25);
+    /**
+     * Tworzy artystyczną, ładną czcionkę. 
+     * @return artystyczna czcionka lub w razie braku - zwykła
+     */
     public static Font makeArtisticFont(){
         String[] fontNames = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
         for(String fontName : fontNames)
