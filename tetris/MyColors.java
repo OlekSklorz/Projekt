87d1b44b5ea01904daffca2ea9eccf37b14b034c
OlekSklorz <olek1995@poczta.onet.pl>
2016-07-07 11:54:48 +0200

    Create MyColors.java
    klasa z kolorami

---
+++ tetris/MyColors.java
@@ -0,0 +1,20 @@
+package tetris;
+
+import static java.awt.Color.*;
+
+/**
+ * Obiekt <code>MyColors</code> reprezentuje podstawowy kolor.
+ * Klasa ta zawiera 12 podstawowych kolorów z klasy Color.
+ */
+class MyColors {
+    public static final java.awt.Color[] colors = {BLUE, CYAN, DARK_GRAY, GRAY, GREEN, LIGHT_GRAY, MAGENTA, ORANGE, PINK, RED, WHITE, YELLOW};
+    
+    /**
+     * Pobiera jeden z 12 kolorów. 
+     * @param i indeks koloru do pobrania
+     * @return kolor
+     */
+    public static java.awt.Color getColor(int i){
+        return colors[i];
+    }
+}
