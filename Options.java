a6c017516e8d3dcd517d64be2d01f191caa9d8a4
Łysy <zuliq94@gmail.com>
2016-07-07 15:45:11 +0200

    Update Options.java

--- Options.java
+++ Options.java
@@ -13,148 +13,144 @@ import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import java.awt.Choice;
+import java.awt.Component;
+import java.awt.GraphicsEnvironment;
 
 /**
  *
  * @author Michał
+ * @param <S>
  */
-public class Options {
-    private JPanel optionsPanel;
-    private String data;
-    private String musicS, fullScreenS, controlS, windowSizeS, fontS;
-    private JTextField musicTF, fullScreenTF, controlTF, windowSizeTF, fontTF;
+public class Options <S>{
+    private JPanel optionsPanel = new JPanel(), controlPanel;
+    private JTextField musicTF = new JTextField("Music"), fullScreenTF = new JTextField("Full Screen"), controlTF = new JTextField("Controls"), 
+            windowSizeTF = new JTextField("Resolution"), fontTF = new JTextField("Fonts");
+    private JButton CreditsButton = new JButton("Credits"), backButton = new JButton("Back"), saveButton = new JButton("Save"), controlButton = new JButton("Controls");
     private JCheckBox fullScreenCB;
-    private static final String creditsS = "duppa";
-    private JButton creditsB, backB, saveB, controlB;
     private Choice musicChoice, windowSizeChoice, fontChoice;
-    
+    private final GridBagConstraints gbc = new GridBagConstraints();
+    private String [] windowSizeTab = new String [3];
+    private String [] musicTab = new String [3];
+    private String [] fontTab = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+    private Object [] allObject = {musicTF, fullScreenTF, controlTF, windowSizeTF, fontTF, controlButton, CreditsButton, saveButton, backButton};
+    private JTextField [] allJTextField = {musicTF, fullScreenTF, controlTF, windowSizeTF, fontTF};
+
+    /*
+    Constructor generate new JPanel with new components. Changing music, type of screen, font, controls.
+    */
     public Options() {
-        Font font = GameFont.makeArtisticFont();
-        optionsPanel = new JPanel();
-        musicTF = makeTField("Music", font, false);
-        fullScreenTF = makeTField("Full Screen", font, false);
-        controlTF = makeTField("Controls", font, false);
-        windowSizeTF = makeTField("Resolution", font, false);
-        fontTF = makeTField("Font", font, false);
-        
-        backB = makeButton("Back", font);
-        creditsB = makeButton("Credits", font);
-        saveB = makeButton("Accept", font);
-        controlB = makeButton("Check", font);
-        
+        Control control = new Control();
+        controlPanel = control.getPanel();
         fullScreenCB = new JCheckBox("", null, false);
         fullScreenCB.setSelected(false);
+ 
+        windowSizeChoice = makeChoice(windowSizeTab);
+        musicChoice = makeChoice(musicTab);
+        fontChoice = makeChoice(fontTab);
         
-        windowSizeChoice = new Choice();
-        windowSizeChoice.add("a");
-        windowSizeChoice.add("b");
-        windowSizeChoice.add("c");
-        
-        musicChoice = new Choice();
-        musicChoice.add("1");
-        musicChoice.add("2");
-        musicChoice.add("3");
-        
-        fontChoice = new Choice();
-        fontChoice.add("!");
-        fontChoice.add("@");
-        fontChoice.add("#");
-        
-        GridBagConstraints gbc = new GridBagConstraints();
+        for(JTextField textfield : allJTextField)
+            textfield.setEditable(false);
+            
         optionsPanel.setLayout(new GridBagLayout());
         gbc.weightx = 100;
         gbc.weighty = 100;
         gbc.gridwidth = 1;
         gbc.gridheight = 1;
         gbc.anchor = GridBagConstraints.CENTER;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        
-        gbc.ipady = 10;
+        gbc.fill = GridBagConstraints.HORIZONTAL;    
         gbc.weightx = 1;
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        optionsPanel.add(musicTF, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        optionsPanel.add(musicChoice, gbc);
-        
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        optionsPanel.add(new JPanel(), gbc);
-        gbc.gridx = 2;
-        gbc.gridy = 0;
-        optionsPanel.add(new JPanel(), gbc);
-        
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        optionsPanel.add(fullScreenTF, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        optionsPanel.add(fullScreenCB, gbc);
-        
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        optionsPanel.add(controlTF, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 2;
-        optionsPanel.add(controlB, gbc);
-        
-        gbc.gridx = 0;
-        gbc.gridy = 3;
-        optionsPanel.add(windowSizeTF, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 3;
-        optionsPanel.add(windowSizeChoice, gbc);
-        
-        gbc.gridx = 0;
-        gbc.gridy = 4;
-        optionsPanel.add(fontTF, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 4;
-        optionsPanel.add(fontChoice, gbc);
-        
-        gbc.gridx = 0;
-        gbc.gridy = 5;
-        gbc.ipady = 20;
-        gbc.gridwidth = 3;
-        optionsPanel.add(creditsB, gbc);
-        gbc.gridx = 0;
-        gbc.gridy = 6;
-        optionsPanel.add(saveB, gbc);
-        gbc.gridx = 0;
-        gbc.gridy = 7;
-        optionsPanel.add(backB, gbc);
 
-        optionsPanel.setVisible(true);
+        int up = 0;
+        for(Object object : allObject) {
+            gbc.gridwidth = 1;
+            placeOnScreen(object, 0, up , 10);
+            if(up == 5)
+                placeOnScreen(object, 1, 2, 10);
+            up++;
+        }
+        placeOnScreen(musicChoice, 1, 0, 10);
+        placeOnScreen(new JPanel(), 2, 0, 10);        
+        placeOnScreen(fullScreenCB, 1, 1, 10);           
+        placeOnScreen(windowSizeChoice, 1, 3, 10);   
+        placeOnScreen(fontChoice, 1, 4, 10);
+
+        optionsPanel.setVisible(true);    
+//------------------------------------------------------------------------------        
+        saveButton.addActionListener(e -> {
+            for(Object object : allObject) {
+                if(object instanceof JTextField) {
+                    JTextField a = (JTextField) object;
+                    a.setFont(Font.decode(getActiveFont()));
+                }
+                if(object instanceof JButton) {
+                    JButton b = (JButton) object;
+                    b.setFont(Font.decode(getActiveFont()));
+                } 
+            }
+        });
         
-        
-        backB.addActionListener(e -> {
+        backButton.addActionListener(e -> {
             optionsPanel.setVisible(false);
             MainMenuFrame.setMainWindowVisable();
         });
-    }
-    
-    public JButton makeButton(String name, Font font) {
-        JButton button = new JButton(name);
-        if(font != null)
-            button.setFont(font);
+//----------------CONTROL        
+        controlButton.addActionListener(e -> {
+            optionsPanel.setVisible(false);
+            controlPanel = control.getPanel();
+        });
         
-        return button;
     }
-    
-    public JTextField makeTField(String name, Font font, Boolean editable) {
-        JTextField textField = new JTextField(name);
-        if(font != null)
-            textField.setFont(font);
-        if(editable != null)
-            textField.setEditable(editable);
-        
-        return textField;
-    }
-    
-    
+  
+    /*
+    Type Choice
+    Returns list of elements (any length) to choice;
+    */
+    public Choice makeChoice(String [] tab) {
+        Choice choice = new Choice();
+        for(int a = 0; a < tab.length; a++) {
+            if(tab[a] == null)
+                tab[a] = "BRAK DANYCH";
+            choice.add(tab[a]); 
+        }
+            
+        return choice;   
+    }   
+    /*
+    Type void
+    Places elements S (any type) on X, Y position;
+    */
+    public void placeOnScreen(Object S, int x, int y, int ipady) {
+        gbc.ipady = ipady;
+        gbc.gridx = x;
+        gbc.gridy = y;  
+        optionsPanel.add((Component) S, gbc);
+    }   
+    /*
+    Type JPanel
+    Returns main panel;
+    */
     public JPanel getJPanel() {
         return optionsPanel;
+    }   
+    /*
+    Type: Button
+    Returns Button;
+    */
+    public JButton getSaveB() {
+        return saveButton;
+    }   
+    /*
+    Type: String
+    Returns selected Font;
+    */
+    public String getActiveFont() {
+        return fontChoice.getSelectedItem();
     }
-    
+    /*
+    Type JButton
+    Return back Button;
+    */
+    public JButton getControlButton() {
+        return controlButton;
+    }
 }
