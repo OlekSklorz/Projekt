b0166f85f8eb84c20a22967eb228d5000aa8efb4
OlekSklorz <olek1995@poczta.onet.pl>
2016-07-09 20:06:00 +0200

    Update SquareRunnable.java

--- tetris/SquareRunnable.java
+++ tetris/SquareRunnable.java
@@ -1,30 +1,44 @@
 package tetris;
 
-import java.awt.Component;
+import java.awt.*;
+import java.util.*;
+import javax.swing.JComponent;
 
 /**
- * Obiekt <code>SquareRunnable</code> reprezentuje poruszanie się kwadratu w 
- * wątku. 
+ * Obiekt <code>GameField</code> reprezentuje główne pole w którym rozgrywa się gra. 
+ * W tym polu rysowane są kolejne elementy gry.
  */
-public class SquareRunnable implements Runnable{
-    private Square square;
-    private Component c;
-    public SquareRunnable(Square square, Component c){
-        this.square = square;
-        this.c = c;
+public class GameField extends JComponent {
+    ArrayList<Square> squares = new ArrayList();
+    public void add(Square square){
+        squares.add(square);
+    }
+    /**
+     * Rysuje wszystkie elementy które występują w grze. Kolor jest wybierany
+     * w sposób losowy. 
+     * @param g obiekt Graphics służący do operacji na figurach. 
+     */
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        for(Square square : squares){
+            g2.setPaint(square.getColor());
+            Element[][] elements = square.getElements();
+            for(int i = 0; i < 2; i++)
+                for(int k = 0; k < 2; k++)
+                    g2.fill(elements[i][k].getElement());
+            g2.setPaint(Color.BLACK);
+            for(int i = 0; i < 2; i++)
+                for(int k = 0; k < 2; k++)
+                    g2.draw(elements[i][k].getElement());
+        }
     }
     
     /**
-     * Porusza kwadratem pionowo i poziomo. Odmalowuje na nowo komponent. 
-     * Ustawia czas wykonywania tego wątku. 
+     * Pobiera preferowany rozmiar pola do gry. 
+     * @return preferowany rozmiar pola do gry. 
      */
-    public void run(){
-        try{
-            for(int i = 1; i < 27; i++){
-                square.move(0, square.getTopX() + Element.getHeight());
-                c.repaint();
-                Thread.sleep(500);
-            }
-        }catch(InterruptedException e){}
+    public Dimension getPreferredSize(){
+        return new Dimension(200, 561);
     }
 }
