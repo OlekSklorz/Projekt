a5fb99875bfde21999ce0c22916fabca63202a7a
OlekSklorz <olek1995@poczta.onet.pl>
2016-07-09 19:41:20 +0200

    Update GameField.java
    dodanie komentarzy

--- tetris/GameField.java
+++ tetris/GameField.java
@@ -4,11 +4,20 @@ import java.awt.*;
 import java.util.*;
 import javax.swing.JComponent;
 
+/**
+ * Obiekt <code>GameField</code> reprezentuje główne pole w którym rozgrywa się gra. 
+ * W tym polu rysowane są kolejne elementy gry.
+ */
 public class GameField extends JComponent {
     ArrayList<Square> squares = new ArrayList();
     public void add(Square square){
         squares.add(square);
     }
+    /**
+     * Rysuje wszystkie elementy które występują w grze. Kolor jest wybierany
+     * w sposób losowy. 
+     * @param g obiekt Graphics służący do operacji na figurach. 
+     */
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
@@ -24,6 +33,11 @@ public class GameField extends JComponent {
                     g2.draw(elements[i][k].getElement());
         }
     }
+    
+    /**
+     * Pobiera preferowany rozmiar pola do gry. 
+     * @return preferowany rozmiar pola do gry. 
+     */
     public Dimension getPreferredSize(){
         return new Dimension(200, 561);
     }
