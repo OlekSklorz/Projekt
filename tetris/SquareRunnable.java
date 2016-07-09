ea52e6bf5e379c96bd5d9a2c79f3403f559a8de3
OlekSklorz <olek1995@poczta.onet.pl>
2016-07-09 19:47:50 +0200

    Update SquareRunnable.java
    dodanie komentarzy, ustawienie zasięgu

--- tetris/SquareRunnable.java
+++ tetris/SquareRunnable.java
@@ -2,13 +2,22 @@ package tetris;
 
 import java.awt.Component;
 
+/**
+ * Obiekt <code>SquareRunnable</code> reprezentuje poruszanie się kwadratu w 
+ * wątku. 
+ */
 public class SquareRunnable implements Runnable{
-    Square square;
-    Component c;
+    private Square square;
+    private Component c;
     public SquareRunnable(Square square, Component c){
         this.square = square;
         this.c = c;
     }
+    
+    /**
+     * Porusza kwadratem pionowo i poziomo. Odmalowuje na nowo komponent. 
+     * Ustawia czas wykonywania tego wątku. 
+     */
     public void run(){
         try{
             for(int i = 1; i < 27; i++){
