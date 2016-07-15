4265032cf0142096154d12708cecd288d6fd0edb
OlekSklorz <olek1995@poczta.onet.pl>
2016-07-15 14:04:36 +0200

    Update Square.java
    metody zwracające liczbę elementów w pionie oraz poziomie

--- tetris/Square.java
+++ tetris/Square.java
@@ -4,11 +4,28 @@ package tetris;
  * Jego wymiary: 2x2. 
  */
 public class Square extends Figure{
+    private static final int yElements = 2, xElements = 2;
     public Square(int leftX, int topX){
-        super(leftX, topX, 2, 2);
+        super(leftX, topX,yElements,xElements);
         elements[0][0] = new Element(leftX, topX);
         elements[1][0] = new Element(leftX, topX + Element.getHeight());
         elements[0][1] = new Element(leftX + Element.getWidth(), topX);
         elements[1][1] = new Element(leftX + Element.getWidth(), topX + Element.getHeight());
+    }
+    
+    /**
+     * Pobiera liczbę elementów budujących figurę w pionie.
+     * @return liczba elementów w pionie. 
+     */
+    public static int getYElements(){
+        return yElements;
+    }
+    
+    /**
+     * Pobiera liczbę elementów budujących figurę w poziomie.
+     * @return liczba elementów w poziomie. 
+     */
+    public static int getXElements(){
+        return xElements;
     }
 }
