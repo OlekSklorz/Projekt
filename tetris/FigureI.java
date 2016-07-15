5fcf33d3481291976be1492a3e5cf164c25c3cef
OlekSklorz <olek1995@poczta.onet.pl>
2016-07-15 14:02:52 +0200

    Update FigureI.java
    metody zwracające liczbę elementów w pionie i poziomie

--- tetris/FigureI.java
+++ tetris/FigureI.java
@@ -4,9 +4,26 @@ package tetris;
  * Jej wymiary to: 4x1.
  */
 public class FigureI extends Figure{
+    private static final int yElements = 4, xElements = 1;
     public FigureI(int leftX, int topX){
-        super(leftX, topX,4,1);
-        for(int i = 0; i < 4; i++)
+        super(leftX, topX,yElements,xElements);
+        for(int i = 0; i < yElements; i++)
             elements[i][0] = new Element(leftX, topX + Element.getHeight() * i);
+    }
+    
+    /**
+     * Pobiera liczbę elementów budujących figurę w pionie.
+     * @return liczba elementów w pionie. 
+     */
+    public static int getYElements(){
+        return yElements;
+    }
+    
+    /**
+     * Pobiera liczbę elementów budujących figurę w poziomie.
+     * @return liczba elementów w poziomie. 
+     */
+    public static int getXElements(){
+        return xElements;
     }
 }
