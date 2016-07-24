36599851270c042054fd6fb1d1135c29f17bb546
OlekSklorz <olek1995@poczta.onet.pl>
2016-07-24 14:28:41 +0200

    Update Levels.java

--- Levels.java
+++ Levels.java
@@ -1,8 +1,8 @@
 package game;
 enum Levels{
-    EASY(1),
-    MEDIUM(2),
-    HARD(3);
+    EASY(0),
+    MEDIUM(1),
+    HARD(2);
     int lvl;
     private Levels(int lvl){
         this.lvl = lvl;
